353ceb7f11e521c467ff1d693c70436a39aa3977
sahahn <sahahn@uvm.edu>
2020-11-05 09:56:23 -0500

    Refactor setup_ML

--- setup_ML.py
+++ setup_ML.py
@@ -32,84 +32,99 @@ def get_datafiles_df():
 def abs_mean(a):
     return np.mean(np.abs(a))
 
-# Setup main ML object with logs
-ML = BPt_ML(log_dr='setup_ML_Logs',
-            existing_log='overwrite',
-            verbose=True,
-            random_state=5,
-            notebook=False,
-            mp_context='fork',
-            n_jobs=32)
+def load_base(ML, show_dist=True):
 
-# Set default NaN values
-ML.Set_Default_Load_Params(na_values=["Don't know", "Decline to answer", "Not sure"])
+    # Set default NaN values
+    ML.Set_Default_Load_Params(na_values=["Don't know", "Decline to answer", "Not sure"])
 
-# Load saved targets df
-targets_df = pd.read_csv('data/targets.csv', index_col='src_subject_id')
+    # Load saved targets df
+    targets_df = pd.read_csv('data/targets.csv', index_col='src_subject_id')
 
-# Move family id to dif df
-family_id_df = targets_df[['rel_family_id']]
+    # Move family id to dif df
+    family_id_df = targets_df[['rel_family_id']]
+    targets_df = targets_df.drop('rel_family_id', axis=1)
 
-# Drop from targets df
-targets_df = targets_df.drop('rel_family_id', axis=1)
+    # Load the targets - with some outlier filtering, but just
+    # setting NaN if outlier.
+    ML.Load_Targets(df=targets_df,
+                    col_name=list(targets_df),
+                    data_type='a',
+                    filter_outlier_std=10,
+                    drop_na=False,
+                    drop_or_na='na',
+                    clear_existing=True)
+ 
+    # Show the distributions for all loaded Targets
+    if show_dist:
+        ML.Show_Targets_Dist()
 
-# Load the targets - with some outlier filtering, but just
-# setting NaN if outlier.
-ML.Load_Targets(df=targets_df,
-                col_name=list(targets_df),
-                data_type='a',
-                filter_outlier_std=10,
-                drop_na=False,
-                drop_or_na='na',
-                clear_existing=True)
+    # Load family id as Strat
+    ML.Load_Strat(df=family_id_df,
+                  col_name='rel_family_id')
 
-# Show the distributions for all loaded Targets
-ML.Show_Targets_Dist()
+    return ML
 
-# Load family id as Strat
-ML.Load_Strat(df=family_id_df,
-              col_name='rel_family_id')
-
-# Load the data files, apply different filtering
-df = get_datafiles_df()
-df.dropna(inplace=True)
-
-ML.Load_Data_Files(df=df[['myelin', 'thick', 'sulc']],
-                   load_func=np.load,
-                   filter_outlier_std=10,
-                   reduce_func=[np.std])
-
-ML.Load_Data_Files(df=df[['curv']],
-                   load_func=np.load,
-                   filter_outlier_std=10,
-                   reduce_func=[np.min, np.max, np.std])
-
-# Save distribution videos
-'''
-for name, func in zip(['Mean', 'Abs. Mean', 'Min', 'Max', 'STD'],
-                    [np.mean, abs_mean, np.min, np.max, np.std]):
-
-    anim = ML.Show_Data_Dist(reduce_func=func, return_anim=True)
-    plt.title(name)
+def load_data(ML, show_dist=False):
     
-    Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=2)
+    # Load the data files, apply different filtering
+    df = get_datafiles_df()
+    df.dropna(inplace=True)
 
-    save_loc = 'setup_ML_Logs/' + name + '.mp4'
-    anim.save(save_loc, dpi=500, writer=writer)
-'''
+    ML.Load_Data_Files(df=df[['myelin', 'thick', 'sulc']],
+                    load_func=np.load,
+                    filter_outlier_std=10,
+                    reduce_func=[np.std])
 
-# Set to no test data
-ML.Train_Test_Split(test_size=0)
+    ML.Load_Data_Files(df=df[['curv']],
+                    load_func=np.load,
+                    filter_outlier_std=10,
+                    reduce_func=[np.min, np.max, np.std])
 
-# Set some verbosity params
-ML.Set_Default_ML_Verbosity(pipeline_verbose=True,
-                            best_params_score=True,
-                            fold_name=True,
-                            time_per_fold=True,
-                            score_per_fold=True,
-                            fold_sizes=True,
-                            best_params=True)
+    # Save distribution videos
+    if show_dist:
+        for name, func in zip(['Mean', 'Abs. Mean', 'Min', 'Max', 'STD'],
+                            [np.mean, abs_mean, np.min, np.max, np.std]):
 
-# Save ML object
-ML.Save('data/Base.ML', low_memory=True)
+            anim = ML.Show_Data_Dist(reduce_func=func, return_anim=True)
+            plt.title(name)
+            
+            Writer = animation.writers['ffmpeg']
+            writer = Writer(fps=2)
+
+            save_loc = 'setup_ML_Logs/' + name + '.mp4'
+            anim.save(save_loc, dpi=500, writer=writer)
+
+def main():
+
+    # Setup main ML object with logs
+    ML = BPt_ML(log_dr='setup_ML_Logs',
+                existing_log='overwrite',
+                verbose=True,
+                random_state=5,
+                notebook=False,
+                mp_context='loky',
+                n_jobs=32)
+
+    # Load targets + strat
+    ML = load_base(ML, show_dist=True)
+
+    # Load the data files, apply different filtering
+    ML = load_data(ML, show_dist=False)
+
+    # Set to no test data
+    ML.Train_Test_Split(test_size=0)
+
+    # Set some verbosity params
+    ML.Set_Default_ML_Verbosity(pipeline_verbose=True,
+                                best_params_score=True,
+                                fold_name=True,
+                                time_per_fold=True,
+                                score_per_fold=True,
+                                fold_sizes=True,
+                                best_params=True)
+
+    # Save ML object
+    ML.Save('data/Base.ML', low_memory=True)
+
+if __name__ == '__main__':
+    main()
