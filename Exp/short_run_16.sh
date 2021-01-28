69e491f913996229e09bfe59450840bda9db56fb
sahahn <sahahn@uvm.edu>
2021-01-28 10:43:40 -0500

    Script

--- Exp/short_run_16.sh
+++ Exp/short_run_16.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #SBATCH --partition=short
 #SBATCH --time=3:00:00
-#SBATCH --mem=24G
-#SBATCH --job-name=24_short
+#SBATCH --mem=16G
+#SBATCH --job-name=16_short
 #SBATCH --output=Job_Logs/%x_%j.out
 #SBATCH --error=Job_Logs/%x_%j.err
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=16
 
-#SBATCH --array=1-100
+#SBATCH --array=1-1000
 
 export OMP_NUM_THREADS=1
 source /users/s/a/sahahn/.bashrc
 
 cd ${SLURM_SUBMIT_DIR}
 
-srun python alt_run.py 24 5
+srun python alt_run.py 16 5
