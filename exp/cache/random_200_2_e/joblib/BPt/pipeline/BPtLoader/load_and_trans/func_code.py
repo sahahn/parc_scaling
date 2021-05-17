53b874e1c297e7191cd7c3f5c3476395bd0b016d
sahahn <sahahn@uvm.edu>
2021-05-17 12:52:59 -0400

    Remove cache and cache fit by mistake

--- exp/cache/random_200_2_e/joblib/BPt/pipeline/BPtLoader/load_and_trans/func_code.py
+++
@@ -1,8 +0,0 @@
-# first line: 13
-def load_and_trans(transformer, load_func, loc):
-    '''This function is designed to be able to be wrapped in a cache
-    check_memory.'''
-
-    data = load_func(loc)
-    trans_data = np.squeeze(transformer.fit_transform(data))
-    return trans_data
